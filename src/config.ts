675ff984b65e734458ee6883880db46a61550227
YAMASHITA Hiromu <hirom.working@gmail.com>
2025-07-29 16:16:36 +0900

    initial

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,11 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
+    website: 'https://hirom.net/', // Site domain
+    title: 'hirom.net', // Site title
+    author: 'YAMASHITA hiromu', // Author name
     description: 'Minimal blog built by Astro', // Site description
-    language: 'en-US' // Default language
+    language: 'ja-JP' // Default language
   },
 
   // GENERAL SETTINGS ////////////////////////////////////////////////////////////////////////////////////
